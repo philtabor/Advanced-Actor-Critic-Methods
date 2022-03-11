92c5f75b2910da47f06528bfc54155e68c199ed4
philtabor <ptabor@gmail.com>
2022-03-10 22:46:11 -0700

    initial commit

---
+++ PPO/continuous/memory.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class PPOMemory:
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.new_states = []
+
+        self.batch_size = batch_size
+
+    def recall(self):
+        return np.array(self.states),\
+            np.array(self.new_states),\
+            np.array(self.actions),\
+            np.array(self.probs),\
+            np.array(self.rewards),\
+            np.array(self.dones)
+
+    def generate_batches(self):
+        n_states = len(self.states)
+        # batch_start = np.arange(0, n_states, self.batch_size)
+        n_batches = int(n_states // self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(indices)
+        # batches = [indices[i:i+self.batch_size] for i in batch_start]
+        batches = [indices[i*self.batch_size:(i+1)*self.batch_size]
+                   for i in range(n_batches)]
+        return batches
+
+    def store_memory(self, state, state_, action, probs, reward, done):
+        self.states.append(state)
+        self.actions.append(action)
+        self.probs.append(probs)
+        self.rewards.append(reward)
+        self.dones.append(done)
+        self.new_states.append(state_)
+
+    def clear_memory(self):
+        self.states = []
+        self.probs = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.new_states = []
---
+++ PPO/continuous/networks.py
@@ -0,0 +1,66 @@
+import os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Beta
+
+
+class ContinuousActorNetwork(nn.Module):
+    def __init__(self, n_actions, input_dims, alpha,
+                 fc1_dims=128, fc2_dims=128, chkpt_dir='models/'):
+        super(ContinuousActorNetwork, self).__init__()
+        self.checkpoint_file = os.path.join(chkpt_dir,
+                                            'actor_continuous_ppo')
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.alpha = nn.Linear(fc2_dims, n_actions)
+        self.beta = nn.Linear(fc2_dims, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = T.tanh(self.fc1(state))
+        x = T.tanh(self.fc2(x))
+        # alpha = F.softplus(self.alpha(x))  + 1.0
+        # beta = F.softplus(self.beta(x))  + 1.0
+        alpha = F.relu(self.alpha(x)) + 1.0
+        beta = F.relu(self.beta(x)) + 1.0
+        dist = Beta(alpha, beta)
+        return dist
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+class ContinuousCriticNetwork(nn.Module):
+    def __init__(self, input_dims, alpha,
+                 fc1_dims=128, fc2_dims=128, chkpt_dir='models/'):
+        super(ContinuousCriticNetwork, self).__init__()
+        self.checkpoint_file = os.path.join(chkpt_dir,
+                                            'critic_continuous_ppo')
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.v = nn.Linear(fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = T.tanh(self.fc1(state))
+        x = T.tanh(self.fc2(x))
+        v = self.v(x)
+
+        return v
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
